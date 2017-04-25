9e75712c6d3104c1a2f2e271a03710e9da7b5e76
joaomosantos <joaomosantos@outlook.com>
2017-04-24 23:35:33 -0300

    Adicionando mensagem erro de acesso em editar perfil

--- backoffice/inc/editar_perfil.php
+++ backoffice/inc/editar_perfil.php
@@ -1,4 +1,5 @@
 <?php if(isset($_REQUEST["editar"]) && isset($_REQUEST["editar"]) === true): ?>
+  
   <?php
     require_once("config.php");
 
@@ -6,22 +7,29 @@
     $perfil -> setNome($_POST["nome"]);
     $perfil -> setEmail($_POST["email"]);
     $statement = $perfil -> editarPerfil($_SESSION["usuario"] -> id);
-    $statement -> execute();
   ?>
-  <?php if($statement -> rowCount() > 0): ?>
-    <div class="alert alert-success" role="alert">
-      <i class="fa fa-check" aria-hidden="true"></i> Perfil alterado com sucesso.
-    </div>
-    <?php 
-      $perfil = new Usuario();
-      $statement = $perfil -> getPerfil($_SESSION["usuario"] -> id);
-      $statement -> execute();
-      $registro = $statement -> fetch(PDO::FETCH_OBJ);
-      $_SESSION["usuario"] = $registro;
-    ?>
+
+  <?php if($statement -> execute()): ?>
+    <?php if($statement -> rowCount() > 0): ?>
+      <div class="alert alert-success" role="alert">
+        <i class="fa fa-check" aria-hidden="true"></i> Perfil alterado com sucesso.
+      </div>
+      <?php 
+        $perfil = new Usuario();
+        $statement = $perfil -> getPerfil($_SESSION["usuario"] -> id);
+        $statement -> execute();
+        $registro = $statement -> fetch(PDO::FETCH_OBJ);
+        $_SESSION["usuario"] = $registro;
+      ?>
+    <?php else: ?>
+      <div class="alert alert-warning" role="alert">
+        <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Não foram feita nenhuma alteração.
+      </div>
+    <?php endif; ?>
   <?php else: ?>
-    <div class="alert alert-warning" role="alert">
-      <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Não foram feita nenhuma alteração.
+    <div class="alert alert-danger" role="alert">
+      <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Falha no acesso.
     </div>
   <?php endif; ?>
+  
 <?php endif; ?>
